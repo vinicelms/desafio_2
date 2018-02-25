173b770e9b5761b77cd82b68902d6c3f89142335
Vinicius Celms <by.vinicius@gmail.com>
2018-02-25 15:53:43 -0300

    Inclusão de data de inicio e termino com validação das datas

--- core/api_consumer.py
+++ core/api_consumer.py
@@ -2,6 +2,7 @@
 
 import requests
 import json
+import datetime
 
 class Api_Consumer():
 
@@ -21,6 +22,21 @@ class Api_Consumer():
             "section" : section,
             "page-size" : qtt_results
         }
+
+        if date_start:
+            try:
+                # Validando valores informados
+                date_start = datetime.datetime.strptime(date_start, "%Y-%m-%d")
+                parameters["from-date"] = datetime.datetime.strftime(date_start, "%Y-%m-%d")
+            except ValueError as e:
+                return "Error in date_start: {}".format(e)
+        if date_end:
+            try:
+                # Validando valores informados
+                date_end = datetime.datetime.strptime(date_end, "%Y-%m-%d")
+                parameters["to-date"] = datetime.datetime.strftime(date_end, "%Y-%m-%d")
+            except ValueError as e:
+                return "Error in date_end: {}".format(e)
 
         req = requests.get(url="{}/search".format(self.BASE_URL), headers=request_headers, params=parameters)
         ret = req.content.decode("utf-8")
